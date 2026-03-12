0e498aed77057c16eb9da9f2ad9504dd49cdc8a3
anastasiapudina <Nastya.pudina@bk.ru>
2026-03-12 08:35:22 +0300

    remove old calculator

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Container, ThemeProvider, createTheme } from "@mui/material"
 //import { RiskCalculator } from "./widgets/RiskCalculator"
 import { ProbabilityCalculator } from "./widgets/ProbabilityCalculator"
 import { observer } from "mobx-react-lite"
-//import { CalculatorType, RootStore } from "./store/store"
-//import Select from "react-select"
 
 const lightTheme = createTheme({
 	palette: {
@@ -15,38 +13,8 @@ const lightTheme = createTheme({
 export const App = observer(() => {
 	return (
 		<ThemeProvider theme={lightTheme}>
-			{/* <Container
-				sx={{
-					display: "flex",
-					height: !RootStore.calculatorType ? "100vh" : "auto",
-					alignItems: "center",
-					justifyContent: "center",
-					marginTop: 1,
-				}}>
-				<Select
-					options={[
-						{
-							value: CalculatorType.Probability,
-							label: "Вероятность прогрессирования",
-						},
-						{
-							value: CalculatorType.Risk,
-							label: "Оценка риска",
-						},
-					]}
-					onChange={item => item && RootStore.setCalculatorType(item.value)}
-					placeholder="Выберите калькулятор"
-				/>
-			</Container> */}
 			<Container sx={{ display: "flex", flexDirection: "row" }}>
 				<ProbabilityCalculator />
-				{/* !!RootStore.calculatorType ? (
-					RootStore.calculatorType === CalculatorType.Probability ? (
-						<ProbabilityCalculator />
-					) : (
-						<RiskCalculator />
-					)
-				) : null */}
 			</Container>
 		</ThemeProvider>
 	)
